0850a366f7e1d36163f00e09f26204a83533b40f
Adriana <43761025+adrianacscg@users.noreply.github.com>
2019-09-22 10:16:19 +0100

    Add files via upload
    todas as alíneas estão resolvidas

--- parque.cpp
+++ parque.cpp
@@ -63,17 +63,50 @@ bool ParqueEstacionamento::entrar(const string &nome) {
     if(vagas == 0)                          //parque está cheio
         return false;
 
-    //atualiza os dados
+
     vagas--;
-    clientes[index].presente = true;
+    clientes[index].presente = true;        //atualiza os dados
 
     return true;
 }
-/*
+
 bool ParqueEstacionamento::retiraCliente(const string &nome) {
-    int index = posicaoCliente(nome);
-    if(clientes[index].presente == true)
+
+    if(posicaoCliente(nome) == -1)      //cliente não existe
         return false;
 
+    int index = posicaoCliente(nome);
+    if(clientes[index].presente == true)                //cliente está ainda dentro do parque
+        return false;
+
+    clientes.erase(clientes.begin()+index);     //apaga o nome do cliente do vetor clientes
+    clientes.erase(clientes.begin()+index);     //apaga o estado do cliente (presente) do vetor
+
+    return true;
+
 }
-*/
+
+bool ParqueEstacionamento::sair(const string &nome) {
+    if(posicaoCliente(nome) == -1)      //cliente não existe
+        return false;
+
+    int index = posicaoCliente(nome);
+    if(clientes[index].presente == false)                //cliente não está dentro do parque
+        return false;
+
+    clientes[index].presente = false;           //atualiza os dados do cliente e vagas
+    vagas ++;
+
+    return true;
+}
+
+unsigned int ParqueEstacionamento::getNumLugaresOcupados() const {
+    int num_lugares = lotacao-vagas;
+    return num_lugares;
+}
+
+unsigned int ParqueEstacionamento::getNumClientesAtuais() const {
+    int num_clientes = clientes.size();
+    return num_clientes;
+}
+
