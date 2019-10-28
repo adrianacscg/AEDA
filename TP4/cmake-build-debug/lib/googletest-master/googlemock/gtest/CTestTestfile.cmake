e95c024531eac6897d5858b52189c81e0ee4fbc3
Adriana Gonçalves <adrianacscg98@gmail.com>
2019-10-28 12:20:12 +0000

    Added TP4

---
+++ TP4/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Adriana/Desktop/FEUP/My Folders/2ano/1semestre/AEDA/4 TP/lib/googletest-master/googletest
+# Build directory: C:/Users/Adriana/Desktop/FEUP/My Folders/2ano/1semestre/AEDA/4 TP/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
